70bf36cd4aeb2b3c953af4f32cdbaaddd82efa24
brady-br <91754190+brady-br@users.noreply.github.com>
2025-04-20 11:46:59 -0400

    fixed redundant dependencies

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang info
 (define name "midilang")
-(define deps '("base" "rackunit-lib" "syntax/parse" "syntax/macro-testing"))
+(define deps '("base" "rackunit-lib"))
 (define license 'MIT)
 (define scribblings '(("scribblings/main.scrbl" () (experimental) "midilang")))
